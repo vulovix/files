95b675c1bdc28a9133f1b13ab8d7fead5fba35eb
IvanVulovic <ivn.vulovic@gmail.com>
2024-02-13 20:26:15 +0100

    added communication provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App/App";
 import "./index.css";
+import CommunicationProvider from "./providers/Communication";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <CommunicationProvider>
+      <App />
+    </CommunicationProvider>
   </React.StrictMode>
 );
